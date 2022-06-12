076602e2581c89a322a3032db27b794a2153bed7
Yocheved Wajsbort <cheviwaj@gmail.com>
2022-06-12 19:18:58 -0400

    Change

--- LakewoodScooper.Web/ClientApp/src/Home.js
+++ LakewoodScooper.Web/ClientApp/src/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Home = () => {
@@ -17,40 +17,42 @@ const Home = () => {
     return (
         <div className='container mt-5'>
             <div className='row'>
-                <img src='https://www.thelakewoodscoop.com/news/wp-includes/images/thelakewoodscoop_logo.png' />
+                <a href='https://www.thelakewoodscoop.com'>
+                    <img src='https://www.thelakewoodscoop.com/news/wp-includes/images/thelakewoodscoop_logo.png' />
+                </a>
                 <h1>  Lakewood Scoop Ad Free!!!!</h1>
             </div>
             <div className='row mt-3'>
                 <div className='col-md-12'>
-                <table className='table table-hover table-striped table-bordered'>
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Image</th>
-                            <th>Text</th>
-                            <th>Comments Count</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                            {posts.map(({link, title, imageUrl, text, commentsCount }, i) => {
-                            return <tr key={i}>
-                                <td>
-                                    <a href={link} target='_blank'>{title}</a>
-                                </td>
-                                 <td>
-                                    {imageUrl && <img src={imageUrl} />}
-                                    {!imageUrl && <h1>No picture</h1>}
-                                </td>
-                                <td>
-                                    {text && `${text}...`}
-                                    {!text && 'Click on the title to read about this'}
-                                </td>
-                                <td>{commentsCount}</td>
+                    <table className='table table-hover table-striped table-bordered'>
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Image</th>
+                                <th>Text</th>
+                                <th>Comments Count</th>
                             </tr>
-                        })}
-                    </tbody>
+                        </thead>
+                        <tbody>
+                            {posts.map(({ link, title, imageUrl, text, commentsCount }, i) => {
+                                return <tr key={i}>
+                                    <td>
+                                        <a href={link} target='_blank'>{title}</a>
+                                    </td>
+                                    <td>
+                                        {imageUrl && <img src={imageUrl} />}
+                                        {!imageUrl && <h1>No picture</h1>}
+                                    </td>
+                                    <td>
+                                        {text && `${text}...`}
+                                        {!text && 'Click on the title to read about this'}
+                                    </td>
+                                    <td>{commentsCount}</td>
+                                </tr>
+                            })}
+                        </tbody>
                     </table>
-                    </div>
+                </div>
             </div>
         </div>
     )
